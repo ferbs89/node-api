9bdcec3468b18c81ae0700a8c6b12288129e1376
Fernando Sanches <ferbs89@gmail.com>
2020-02-12 23:00:53 -0300

    Teste de envio de e-mail utilizando Nodemailer

--- src/config/database.js
+++ src/config/database.js
@@ -1,20 +1,18 @@
-require("dotenv").config();
-
 module.exports = {
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: true
-  },
-  operatorsAliases: false,
-  logging: false,
-  define: {
-    timestamps: true,
-    paranoid: true,
-    underscored: true,
-    underscoredAll: true,
-  }
+	host: process.env.DB_HOST,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
+	dialect: "postgres",
+	dialectOptions: {
+		ssl: true
+	},
+	operatorsAliases: false,
+	logging: false,
+	define: {
+		timestamps: true,
+		paranoid: true,
+		underscored: true,
+		underscoredAll: true,
+	}
 };
