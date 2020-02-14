83ddf3b8288181ad39122d871a09cef4623c064c
Fernando Belmonte Sanches <ferbs89@gmail.com>
2020-02-14 17:50:17 -0300

    Nodemailer rate limit

--- src/config/mail.js
+++ src/config/mail.js
@@ -3,6 +3,9 @@ module.exports = {
     port: process.env.MAIL_PORT,
     auth: {
         user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS
-    }
+        pass: process.env.MAIL_PASS,
+    },
+    pool: true,
+    rateDelta: 15000,
+    rateLimit: true,
 };
